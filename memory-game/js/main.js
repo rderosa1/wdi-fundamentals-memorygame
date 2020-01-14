44091825cc9c81dac7654414bae538ee60bcfc82
Ricardo DeRosa <rico@Ricardos-MacBook-Pro.local>
2020-01-14 10:14:55 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,33 @@
 console.log("Up and running!");
 
 
-const cards = ["queen", "queen", "king", "king"];
+// const cards = ["queen", "queen", "king", "king"];
+
+const cards = [ 
+	{ 
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+	},
+
+	{ 
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+	},
+
+	{ 
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+	},
+
+	{ 
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+	}, 
+];
 
 const cardsInPlay = [];
 
@@ -15,9 +41,12 @@ function checkForMatch(){
 
 function flipCard(cardId){
 
-	console.log("User has flipped " + cards[cardId]);	
+	console.log("User has flipped " + cards[cardId].rank);	
 
-	 cardsInPlay.push(cards[cardId]);
+	 cardsInPlay.push(cards[cardId].rank);
+
+	 console.log(cards[cardId].cardImage);
+	 console.log(cards[cardId].suit);
 
 if (cardsInPlay.length === 2) {
 
